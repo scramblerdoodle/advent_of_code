4976022ce4bffccc98ba7883ce45b878732c4cac
Pedro Orlando <porlando@riseup.net>
2025-04-28 01:24:02 -0300

    [AdvOfCode24][Day 16] Part 2: WIP (how to overengineer things)
    And not even be able to solve the problem

--- advent_of_code_24/src/day16.rs
+++ advent_of_code_24/src/day16.rs
@@ -2,12 +2,13 @@ use std::{fmt, fs::read_to_string};
 
 use crate::utils::{get_test_file, Board, Direction, FileNotFound, ACTUAL, EXAMPLE};
 
+#[derive(Debug)]
 enum State {
     Start,
-    End,
+    End(bool, Vec<(usize, usize)>),
     Wall,
     Empty,
-    Visited(Direction, u32),
+    Visited(Direction, u32, bool, Vec<(usize, usize)>),
 }
 
 impl State {
@@ -16,7 +17,7 @@ impl State {
             '#' => State::Wall,
             '.' => State::Empty,
             'S' => State::Start,
-            'E' => State::End,
+            'E' => State::End(true, vec![]),
             _ => panic!("Unexpected symbol in input"),
         }
     }
@@ -26,8 +27,8 @@ impl State {
             State::Wall => '#',
             State::Empty => '.',
             State::Start => 'S',
-            State::End => 'E',
-            State::Visited(d, _) => d.as_char(),
+            State::End(_, _) => 'E',
+            State::Visited(d, _, _, _) => d.as_char(),
         }
     }
 }
@@ -46,16 +47,35 @@ impl fmt::Display for Board<State> {
     }
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug, Clone, Copy)]
 struct Node {
     score: u32,
     pos: (usize, usize),
     dir: Direction,
 }
 
+impl Node {
+    fn from_visited(state: &State, pos: (usize, usize)) -> Option<Node> {
+        match state {
+            State::Visited(dir, score, _, _) => Some(Node {
+                score: *score,
+                pos,
+                dir: *dir,
+            }),
+            State::Start => Some(Node {
+                score: 0,
+                pos,
+                dir: Direction::Right,
+            }),
+            _ => None,
+        }
+    }
+}
+
 struct Input {
     board: Board<State>,
     start: (usize, usize),
+    end: (usize, usize),
     minimum_score: u32,
 }
 
@@ -63,11 +83,13 @@ impl Input {
     fn new(board: Vec<Vec<State>>) -> Self {
         let board = Board::new(board);
         let mut start = (0, 0);
+        let mut end = (0, 0);
 
         for i in 0..board.len() {
             for j in 0..board[i].len() {
                 match Some(board.get_pos((i, j)).unwrap()) {
                     Some(State::Start) => start = (i, j),
+                    Some(State::End(_, _)) => end = (i, j),
                     _ => (),
                 }
             }
@@ -76,6 +98,7 @@ impl Input {
         Self {
             board,
             start,
+            end,
             minimum_score: u32::MAX,
         }
     }
@@ -83,11 +106,10 @@ impl Input {
     fn update_min(&mut self, other: u32) {
         if self.minimum_score > other {
             self.minimum_score = other;
-
-            // println!("Min: {}\n{}", self.minimum_score, self.board);
         }
     }
-    fn walk(&mut self, start: (usize, usize)) {
+
+    fn walk(&mut self, start: (usize, usize), end: (usize, usize)) {
         /* Approach:
          *   All visited positions in board will have a score
          *   Queue structure: Vec<(score, pos<usize, usize>)>
@@ -143,6 +165,10 @@ impl Input {
 
                 for d in &Direction::ORTHOGONALS {
                     let next_pos = self.board.add_direction(d, curr_pos).unwrap();
+                    if next_pos == end && curr_score < self.minimum_score {
+                        self.update_min(curr_score + 1);
+                    }
+
                     let next_state = self.board.get_pos(next_pos).unwrap();
                     // println!(
                     //     "Dir: {:?}, Next pos: {:?}, Next state: {:?}",
@@ -163,17 +189,20 @@ impl Input {
 
                             // If score goes beyond minimum score, no use computing it
                             if new_score < self.minimum_score {
-                                self.board
-                                    .update_pos(next_pos, State::Visited(*d, new_score));
-
-                                queue.push(Node {
+                                let node = Node {
                                     score: new_score,
                                     pos: next_pos,
                                     dir: *d,
-                                });
+                                };
+                                self.board.update_pos(
+                                    next_pos,
+                                    State::Visited(*d, new_score, false, vec![curr_pos]),
+                                );
+
+                                queue.push(node);
                             }
                         }
-                        State::Visited(d, s) => {
+                        State::Visited(d, s, _, prev) => {
                             // Compute score
                             let mut new_score = curr_score;
                             if *d != prev_dir {
@@ -181,22 +210,30 @@ impl Input {
                             }
                             new_score += 1;
 
-                            // If score goes beyond minimum score or visited already has lower
-                            // score, no use computing it
-                            if new_score < *s && new_score < self.minimum_score {
-                                // Add to queue (score, pos)
-                                queue.push(Node {
-                                    score: new_score,
-                                    pos: next_pos,
-                                    dir: d.clone(),
-                                });
+                            let node = Node {
+                                score: new_score,
+                                pos: next_pos,
+                                dir: *d,
+                            };
+                            if new_score <= *s {
+                                // Save previous nodes
+                                let mut new_prev = prev.clone();
+                                new_prev.push(curr_pos);
+                                self.board.update_pos(
+                                    next_pos,
+                                    State::Visited(*d, new_score, false, new_prev),
+                                );
 
-                                self.board
-                                    .update_pos(next_pos, State::Visited(*d, new_score));
+                                // Add to queue (score, pos)
+                                queue.push(node);
                             }
                         }
-                        State::End => {
+                        State::End(_, prev) => {
                             // Set minimum score, with sanity check
+                            let mut new_prev = prev.clone();
+                            new_prev.push(curr_pos);
+                            self.board.update_pos(next_pos, State::End(true, new_prev));
+
                             if curr_score < self.minimum_score {
                                 self.update_min(curr_score + 1);
                             }
@@ -206,27 +243,129 @@ impl Input {
             }
         }
     }
+
+    fn backtrack(&mut self) {
+        // Run the maze backwards -- start at end
+        // queue: Vec<&NodeBacktrack> or Vec<Box<NodeBacktrack>>
+        // Add end to queue
+        //
+        // While queue not empty
+        //  node = pop from queue
+        //  Set node as optimal
+        //  Get min score in node.prev
+        //  For each node with min score
+        //      Add to queue
+        //
+        // Count all nodes set as optimal
+        let mut queue: Vec<(usize, usize)> = vec![self.end];
+
+        println!("Backtracking");
+
+        while let Some(pos) = queue.pop() {
+            let node = self.board.get_pos(pos).unwrap();
+            println!("{:?}", node);
+
+            match node {
+                State::End(_, prev) | State::Visited(_, _, _, prev) => {
+                    let possible_nodes: Vec<Node> = prev
+                        .iter()
+                        .map(|&pos| {
+                            Node::from_visited(self.board.get_pos(pos).unwrap(), pos).unwrap()
+                        })
+                        .collect();
+
+                    println!("{:?}", possible_nodes);
+                    let min_score = possible_nodes.iter().min_by_key(|x| x.score).unwrap().score;
+                    let min_score_nodes: Vec<&Node> = possible_nodes
+                        .iter()
+                        .filter(|n| n.score == min_score)
+                        .collect();
+
+                    for pos in min_score_nodes.iter().map(|n| n.pos) {
+                        queue.push(pos);
+                    }
+
+                    if let State::Visited(d, s, _, _) = node {
+                        self.board
+                            .update_pos(pos, State::Visited(*d, *s, true, vec![]));
+                    }
+                }
+                _ => {}
+            };
+        }
+    }
 }
 
 fn day16(mut input: Input) -> u32 {
-    input.walk(input.start);
+    input.walk(input.start, input.end);
     input.minimum_score
 }
 
-fn day16_v2(_input: Input) -> u32 {
-    let result: u32 = 0;
+fn day16_v2(mut input: Input) -> u32 {
+    input.walk(input.start, input.end);
+
+    for i in 0..input.board.len() {
+        for j in 0..input.board[i].len() {
+            if let State::End(_op, prev) = input.board.get(i).unwrap().get(j).unwrap() {
+                println!("End: prev: {:?}, pos: ({i},{j})", prev);
+            }
+        }
+    }
+    println!("{}", input.board);
+
+    println!("{:?}", input.end);
+    input.backtrack();
+    print_optimal(&input.board);
+
+    let mut result: u32 = 0;
+
+    for v in input.board {
+        for s in v {
+            result += match s {
+                State::Visited(_, _, optim, _) => {
+                    if optim {
+                        1
+                    } else {
+                        0
+                    }
+                }
+                _ => 0,
+            };
+        }
+    }
+
     result
 }
 
+fn print_optimal(input: &Board<State>) {
+    println!(
+        concat!("Board:\n\t{}"),
+        input
+            .board
+            .iter()
+            .map(|v| v
+                .iter()
+                .map(|s| {
+                    if let State::Visited(_, _, optim, _) = s {
+                        if *optim {
+                            'O'
+                        } else {
+                            '.'
+                        }
+                    } else {
+                        s.to_char()
+                    }
+                })
+                .collect())
+            .collect::<Vec<String>>()
+            .join("\n\t"),
+    )
+}
 fn parse_input(filepath: &str) -> Input {
     let mut board: Vec<Vec<State>> = vec![];
 
     read_to_string(filepath).unwrap().lines().for_each(|l| {
-        board.push(
-            l.chars()
-                .map(|c| State::from_char(c))
-                .collect::<Vec<State>>(),
-        );
+        board.push(l.chars().map(State::from_char).collect::<Vec<State>>());
     });
 
     Input::new(board)
@@ -278,11 +417,11 @@ mod tests {
 
     #[test]
     fn test_example_1_v2() {
-        assert_eq!(main("example_v2").unwrap(), 0);
+        assert_eq!(main("example_v2").unwrap(), 45);
     }
 
     #[test]
     fn test_example_2_v2() {
-        assert_eq!(main("example2_v2").unwrap(), 0);
+        assert_eq!(main("example2_v2").unwrap(), 64);
     }
 }
