df2e3f48c1b79fa771d751729b3ee72673567512
Pedro Orlando <porlando@riseup.net>
2026-01-15 15:43:43 -0300

    [AoC25] Day 4 pt 2

--- advent_of_code_25/pkg/day04.go
+++ advent_of_code_25/pkg/day04.go
@@ -39,7 +39,41 @@ func day4_pt1(input []string) int {
 }
 
 func day4_pt2(input []string) int {
-	return 0
+	board := utils.NewBoardFromValues(utils.Make2DRuneSlice(input))
+	acc := 0
+	to_remove := 0
+
+	// utils.PrintBoard(board)
+
+	for ok := true; ok; ok = to_remove > 0 {
+		to_remove = 0
+		for y := 0; y < board.Y; y++ {
+			for x := 0; x < board.X; x++ {
+				current := board.Board[y][x]
+
+				if current == '@' {
+					neighbours := utils.CheckNeighbours(board, x, y, rune('!'))
+					// utils.PrintNeighbours(neighbours)
+					neighbours_slice := utils.NeighboursToSlice(neighbours)
+
+					tp_rolls := 0
+					for _, n := range neighbours_slice {
+						if n == '@' {
+							tp_rolls++
+						}
+					}
+
+					if tp_rolls < 4 {
+						to_remove++
+						board.Board[y][x] = '.'
+					}
+				}
+			}
+		}
+		acc += to_remove
+	}
+
+	return acc
 }
 
 func Day04() {
