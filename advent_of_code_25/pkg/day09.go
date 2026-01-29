8f1450663c12c32c73530f7ee0a169f4455a26e6
Pedro Orlando <porlando@riseup.net>
2026-01-29 16:05:41 -0300

    [AoC25] [WIP] Day 9 fail

--- advent_of_code_25/pkg/day09.go
+++ advent_of_code_25/pkg/day09.go
@@ -119,10 +119,11 @@ func day9_pt2(input []string) int {
 	utils.DebugPrintln("Filling up grid areas")
 	for i := 0; i < len(grid.Grid); i++ {
 		for j := 0; j < len(grid.Grid[i]); j++ {
-			if grid.Grid[i][j] {
+			if grid.Grid[i][j] && !grid.Grid[i][j+1] && grid.Grid[i-1][j+1] {
 				if slices.Contains(grid.Grid[i][j+1:], true) {
 					for k := j + 1; ; k++ {
 						if grid.Grid[i][k] {
+							j = k + 1
 							break
 						}
 						grid.Grid[i][k] = true
@@ -133,20 +134,20 @@ func day9_pt2(input []string) int {
 	}
 	if len(coordinates) < 100 {
 		grid.PrintGrid()
-
 	}
 
 	utils.DebugPrintln("Computing rectangles")
 	max_area := 0.0
 	for i, r1 := range coordinates {
-		utils.DebugPrintln("Evaluating rectangles starting from coordinate index", i)
-		utils.DebugPrintln("Current max area:", max_area)
 		for _, r2 := range coordinates[i+1:] {
+			utils.DebugPrintln("Evaluating", r1, r2)
 			area := math.Abs(float64(r1.X-r2.X+1)) * math.Abs(float64(r1.Y-r2.Y+1))
 			// Right off the bat, if area is lower than max, don't even check
-			if area < max_area {
+			if area <= max_area {
 				continue
 			}
+			utils.DebugPrintln("Evaluating rectangles starting from coordinate index", i)
+			utils.DebugPrintln("Current max area:", max_area)
 
 			// if r1.X < r2.X {
 			// 	if !grid.Grid[r1.Y][r1.X+1] || !grid.Grid[r2.Y][r2.X-1] {
@@ -173,18 +174,142 @@ func day9_pt2(input []string) int {
 			lowest_y := min(r1.Y, r2.Y)
 			highest_y := max(r1.Y, r2.Y)
 
-			for j1 := lowest_y; j1 < highest_y && valid; j1++ {
-				for j2 := lowest_x; j2 < highest_x && valid; j2++ {
-					if !grid.Grid[j1][j2] {
-						valid = false
+			for _, r3 := range coordinates {
+				// If coordinate fall within rectangle
+				if lowest_x <= r3.X && r3.X <= highest_x && lowest_y <= r3.Y && r3.Y <= highest_y {
+					// Check if the surroundings that fall within the rectangle are valid
+					// i.e. .#XXXXX#.
+					// 			.XXXXXXX.
+					// 			.XX#X#XX.
+					// 			.XXX.XXX.
+					// 			.#X#.@X#.
+					// Checking rectangle for top-left most and down-right most
+					// would have all coordinates.
+					// Once we check @, its left-neighbour would be inside the rectangle
+					// but outside the valid area
+
+					neighbours := grid.GetNeighbours(r3.X, r3.Y, false)
+					// r3 is the lowest X coord, right has to be valid
+					if r3.X == lowest_x {
+						if !neighbours.E {
+							utils.DebugPrintln("Invalid at lowest_x because of", r3)
+							valid = false
+							break
+						}
+					}
+					if r3.Y == lowest_y {
+						if !neighbours.S {
+							utils.DebugPrintln("Invalid at lowest_y because of", r3)
+							valid = false
+							break
+						}
+					}
+					if r3.X == highest_x {
+						if !neighbours.W {
+							utils.DebugPrintln("Invalid at highest_x because of", r3)
+							valid = false
+							break
+						}
+					}
+					if r3.Y == highest_y {
+						if !neighbours.N {
+							utils.DebugPrintln("Invalid at highest_y because of", r3)
+							valid = false
+							break
+						}
+					}
+					if lowest_x < r3.X && r3.X < highest_x {
+						// r3 is right in the middle of X coordinates, left and right have to be valid:
+						if !neighbours.W || !neighbours.E {
+							utils.DebugPrintln("Invalid between lowest_x and highest_x because of", r3)
+							valid = false
+							break
+						}
+					} else if lowest_y < r3.Y && r3.Y < highest_y {
+						// r3 is right in the middle of Y coordinates, up and down have to be valid:
+						if !neighbours.N || !neighbours.S {
+							utils.DebugPrintln("Invalid between lowest_y and highest_y because of", r3)
+							valid = false
+							break
+						}
+					} else if r3.X == lowest_x {
+						// r3 is on the same column as lowest X
+						// If below, up and right have to valid
+						if r3.Y > lowest_y {
+							if !neighbours.N || !neighbours.E {
+								utils.DebugPrintln("Invalid at lowest_x and >= lowest_y because of", r3)
+								valid = false
+								break
+							}
+						}
+						// If above, down and right have to be valid
+						if r3.Y < highest_y {
+							if !neighbours.S || !neighbours.E {
+								utils.DebugPrintln("Invalid at lowest_x and <= highest_y because of", r3)
+								valid = false
+								break
+							}
+						}
+					} else if r3.X == highest_x {
+						// r3 is on the same column as highest X
+						// If below, up and left have to valid
+						if r3.Y > lowest_y {
+							if !neighbours.N || !neighbours.W {
+								utils.DebugPrintln("Invalid at highest_x and >= lowest_y because of", r3)
+								valid = false
+								break
+							}
+						}
+						// If above, down and left have to valid
+						if r3.Y < highest_y {
+							if !neighbours.S || !neighbours.W {
+								utils.DebugPrintln("Invalid at highest_x and <= highest_y because of", r3)
+								valid = false
+								break
+							}
+						}
+					} else if r3.Y == lowest_y {
+						// r3 is on the same row as lowest Y
+						// If to the right, down and left have to valid
+						if r3.X > lowest_x {
+							if !neighbours.S || !neighbours.W {
+								utils.DebugPrintln("Invalid at lowest_y and >= lowest_x because of", r3)
+								valid = false
+								break
+							}
+						}
+						// If to the left, down and right have to valid
+						if r3.X < highest_x {
+							if !neighbours.S || !neighbours.E {
+								utils.DebugPrintln("Invalid at lowest_y and <= highest_x because of", r3)
+								valid = false
+								break
+							}
+						}
+					} else if r3.Y == highest_y {
+						// r3 is on the same row as highest Y
+						// If to the right, up and left have to valid
+						if r3.X > lowest_x {
+							if !neighbours.N || !neighbours.W {
+								utils.DebugPrintln("Invalid at highest_y and >= lowest_x because of", r3)
+								valid = false
+								break
+							}
+						}
+						// If to the left, up and right have to valid
+						if r3.X < highest_x {
+							if !neighbours.N || !neighbours.E {
+								utils.DebugPrintln("Invalid at highest_y and <= highest_x because of", r3)
+								valid = false
+								break
+							}
+						}
 					}
 				}
 			}
-			if !valid {
-				continue
-			}
 
-			if area > max_area {
+			if area > max_area && valid {
+				utils.DebugPrintln(r1, r2)
 				max_area = area
 			}
 		}
@@ -201,5 +326,7 @@ func Day09() {
 
 	ret = day9_pt2(input)
 	// Ret: 227040352, too low
+	// Ret: 4739623064, too high
+	// Ret: 4684846992, too high
 	fmt.Println(ret)
 }
