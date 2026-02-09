762cff85300735a3f597fb6bab8b6d9bce873e2e
Pedro Orlando <porlando@riseup.net>
2026-02-09 17:13:09 -0300

    [AoC25] Day 6 part 2

--- advent_of_code_25/pkg/day06.go
+++ advent_of_code_25/pkg/day06.go
@@ -52,7 +52,78 @@ func day6_pt1(input []string) int {
 }
 
 func day6_pt2(input []string) int {
+	// Take input as-is, just convert to runes to handle them as chars
+	formatted_input := [][]rune{}
+	for _, line := range input {
+		formatted_input = append(formatted_input, []rune(line))
+	}
+
+	grid := utils.NewGridFromValues(formatted_input)
+	max_Y := len(grid.Grid)
+	max_X := len(grid.Grid[0])
+
 	acc := 0
+	digits_col := []int{}
+XLoop:
+	for x := max_X - 1; x >= 0; x-- {
+
+		// Variable to build the numbers column-wise
+		digit_str := []rune{}
+		for y := range max_Y {
+			c := grid.Grid[y][x]
+
+			switch c {
+			case ' ':
+				// do nothing
+
+			case '+', '*':
+				// run operation on column's numbers
+
+				// add last built number to the column digits
+				d, _ := strconv.Atoi(string(digit_str))
+				digits_col = append(digits_col, d)
+				utils.DebugPrintln("digits_col:", digits_col)
+
+				// Compute operation
+				result := 0
+				if c == '+' {
+					for _, n := range digits_col {
+						result += n
+					}
+					utils.DebugPrintln("result:", result)
+
+				} else {
+					result = 1
+					for _, n := range digits_col {
+						result *= n
+					}
+					utils.DebugPrintln("result:", result)
+
+				}
+
+				// Add to acc
+				acc += result
+
+				// empty digits_col
+				digits_col = []int{}
+
+				// next column is empty so skip it
+				x--
+
+				// also skip to next x loop otherwise we would add the current digit back
+				continue XLoop
+
+			default:
+				// append to digits_str
+				digit_str = append(digit_str, c)
+			}
+		}
+
+		// Append accumulated digit into digits_col for the final operation
+		d, _ := strconv.Atoi(string(digit_str))
+		digits_col = append(digits_col, d)
+		utils.DebugPrintln("digits_col:", digits_col)
+	}
 	return acc
 }
 
